f8d44652788d496c768191d4856d8408dadb5226
ferrenkalalo <s21910001@student.unklab.ac.id>
2022-05-06 23:58:27 +0800

    function add photo and flash message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import Routers from './routers';
+import FlashMessage from 'react-native-flash-message';
 
 const App = () => {
   return (
     <NavigationContainer>
       <Routers />
+      <FlashMessage position="top" />
     </NavigationContainer>
   );
 };
--- src/components/molecules/Header/index.js
+++ src/components/molecules/Header/index.js
@@ -2,11 +2,38 @@ import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import React, {useState} from 'react';
 import Gap from '../../atoms/Gap';
 import {Profile} from '../../../assets';
+import {launchImageLibrary} from 'react-native-image-picker';
+import {showMessage} from 'react-native-flash-message';
 
 const Header = () => {
+  const [photo, setPhoto] = useState('');
+  const [hasPhoto, setHasPhoto] = useState(false);
+  const getPhoto = async () => {
+    const result = await launchImageLibrary({
+      maxHeight: 100,
+      maxWidth: 100,
+      includeBase64: true,
+    });
+    if (result.didCancel) {
+      setHasPhoto(false);
+      showMessage({
+        message: 'CANCELED!',
+        type: 'default',
+        backgroundColor: '#FFC7C7',
+        color: 'black',
+      });
+    } else {
+      setPhoto(result.assets[0].uri);
+      setHasPhoto(true);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Profile width="100" height="100" />
+      <TouchableOpacity onPress={getPhoto}>
+        {!hasPhoto && <Profile width="100" height="100" />}
+        {hasPhoto && <Image source={{uri: photo}} style={styles.avatar} />}
+      </TouchableOpacity>
       <Gap width={50} height={50} />
     </View>
   );
@@ -20,5 +47,10 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  avatar: {
+    height: 100,
+    width: 100,
+    borderRadius: 100,
   },
 });
