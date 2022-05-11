af183c80d15124f81ac9ae2d049edd66ce3d42eb
aprayielmandak <s21910554@student.unklab.ac.id>
2022-05-11 13:13:05 +0800

    fix images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,69 @@
+import DefaultImage from './DefaultImage.png';
 import BouquetCharacter from './BouquetCharacter.png';
 import BouquetFlower from './BouquetFlower.png';
 import BouquetMoney from './BouquetMoney.png';
+import MinnieMouse from './MinnieMouse.png';
+import MickeyMouse from './MickeyMouse.png';
+import PatrickStar from './PatrickStar.png';
+import Spongebob from './Spongebob.png';
+import Stitch from './Stitch.png';
+import WhiteFlower from './WhiteFlower.png';
+import YellowFlower from './YellowFlower.png';
+import GreenFlower from './GreenFlower.png';
+import RedFlower from './RedFlower.png';
+import Pecahan10 from './Pecahan10.png';
+import Pecahan20 from './Pecahan20.png';
+import Pecahan50 from './Pecahan50.png';
+import Pecahan100 from './Pecahan100.png';
+import GreenRibbon from './GreenRibbon.png';
+import WhiteRibbon from './WhiteRibbon.png';
+import BlackRibbon from './BlackRibbon.png';
+import RedRibbon from './RedRibbon.png';
+import BluePapper from './BluePapper.png';
+import BlackPapper from './BlackPapper.png';
+import PinkPapper from './PinkPapper.png';
+import PurplePapper from './PurplePapper.png';
+import CardNav from './CardNav.png';
+import PapperNav from './PaperNav.png';
+import RibbonNav from './RibbonNav.png';
+import FlowerNav from './FlowerNav.png';
+import CharacterNav from './CharacterNav.png';
+import MoneyNav from './MoneyNav.png';
+import NavF from './NavF.png';
+import BackIcon from './BackIcon.png';
 
-export {BouquetCharacter, BouquetFlower, BouquetMoney};
+export {
+  DefaultImage,
+  BouquetCharacter,
+  BouquetFlower,
+  BouquetMoney,
+  MinnieMouse,
+  MickeyMouse,
+  PatrickStar,
+  Spongebob,
+  Stitch,
+  WhiteFlower,
+  YellowFlower,
+  GreenFlower,
+  RedFlower,
+  Pecahan10,
+  Pecahan20,
+  Pecahan50,
+  Pecahan100,
+  GreenRibbon,
+  WhiteRibbon,
+  BlackRibbon,
+  RedRibbon,
+  BluePapper,
+  BlackPapper,
+  PinkPapper,
+  PurplePapper,
+  RibbonNav,
+  PapperNav,
+  CardNav,
+  FlowerNav,
+  CharacterNav,
+  MoneyNav,
+  NavF,
+  BackIcon,
+};
